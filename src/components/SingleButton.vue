eb8699912cb45b116c4de3879457eafb6132cf3f
Alireza Sadeghi <alirezawdx@gmail.com>
2025-07-05 14:12:33 +0330

    feat: add singe button component

---
+++ src/components/SingleButton.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const props = defineProps({
+  href: String,
+})
+</script>
+
+<template>
+  <a
+    :href="href"
+    class="border-2 border-[#484E53] rounded-lg py-2 px-5 font-light hover:bg-[#4FC3F7] hover:border-[#4FC3F7] duration-150 hover:text-white"
+  >
+    <slot></slot>
+  </a>
+</template>
