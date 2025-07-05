ace48e24ee30284dee5c35f9766e12caec2c789d
Alireza Sadeghi <alirezawdx@gmail.com>
2025-07-05 13:36:20 +0330

    feat: add navbar menu items and list components

---
+++ src/components/MenuItem.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const props = defineProps({
+  href: String,
+})
+</script>
+
+<template>
+  <li>
+    <a :href="props.href" class="text-[#050505] hover:text-[#4FC3F7] duration-150 p-0 m-0">
+      <slot></slot>
+    </a>
+  </li>
+</template>
---
+++ src/components/MenuList.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import MenuItem from './MenuItem.vue'
+
+const props = defineProps({
+  links: Array,
+})
+</script>
+
+<template>
+  <ul class="flex items-center">
+    <MenuItem v-for="link in props.links" :href="link.href">
+      {{ link.text }}
+    </MenuItem>
+  </ul>
+</template>
