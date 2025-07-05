e1b96119b3a2a1896a70f9cdd0d5a4691d6bc77c
Alireza Sadeghi <alirezawdx@gmail.com>
2025-07-05 15:52:44 +0330

    refactor: split code from App to HomeView

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
-import NavbarMenu from './layouts/NavbarMenu.vue'
+import HomeView from './views/HomeView.vue'
 </script>
 
 <template>
-  <div class="container mx-auto py-12">
-    <NavbarMenu />
-  </div>
+  <HomeView />
 </template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import NavbarMenu from '../layouts/NavbarMenu.vue'
+</script>
+
+<template>
+  <div class="container mx-auto py-12">
+    <NavbarMenu />
+  </div>
+</template>
