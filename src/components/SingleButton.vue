9668e2ac1795325b6e3106c7715eb0c319db97d8
Alireza Sadeghi <alirezawdx@gmail.com>
2025-07-05 20:48:49 +0330

    refactor: format single button

--- src/components/SingleButton.vue
+++ src/components/SingleButton.vue
@@ -8,7 +8,7 @@ const props = defineProps({
 <template>
   <a
     :href="href"
-    :class="shape == 'pill' ? 'rounded-full py-3 w-44' : 'rounded-lg py-2 px-5'"
+    :class="shape == 'pill' ? 'rounded-full py-3 w-52' : 'rounded-lg py-2 px-5'"
     class="border-2 border-[#484E53] font-light hover:bg-[#4FC3F7] hover:border-[#4FC3F7] duration-150 hover:text-white"
   >
     <slot></slot>
