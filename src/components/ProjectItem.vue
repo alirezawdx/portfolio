63b171d60645efee5b4ef28cb6ff7e73f781382d
Alireza Sadeghi <alirezawdx@gmail.com>
2025-07-06 11:07:27 +0330

    feat: add project item component

---
+++ src/components/ProjectItem.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import SingleButton from './SingleButton.vue'
+
+const { img, tech } = defineProps({ img: String, tech: String })
+</script>
+
+<template>
+  <div
+    class="text-left border border-transparent hover:border-[#484E53] duration-200 rounded-2xl hover:shadow-2xl hover:-translate-y-2"
+  >
+    <img :src="img" alt="" class="rounded-xl shadow-xl" />
+
+    <div class="flex justify-between items-center mt-5">
+      <div>
+        <h4 class="text-xl font-semibold text-[#484E53]">
+          <slot></slot>
+        </h4>
+        <p class="text-[#484E53">{{ tech }}</p>
+      </div>
+      <SingleButton href="/meow">view</SingleButton>
+    </div>
+  </div>
+</template>
