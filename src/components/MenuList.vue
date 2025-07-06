fef0e7228c558adf4f5a7c1a394543cb4892f155
Alireza Sadeghi <alirezawdx@gmail.com>
2025-07-06 12:37:57 +0330

    fix: fix vue iteration on menu list

--- src/components/MenuList.vue
+++ src/components/MenuList.vue
@@ -1,16 +1,19 @@
 <script setup lang="ts">
 import MenuItem from './MenuItem.vue'
 
-const props = defineProps({
-  links: Array,
-})
+const props = defineProps<{
+  links: Array<{
+    href: string
+    text: string
+  }>
+}>()
 </script>
 
 <template>
   <ul
     class="flex items-start md:items-center justify-start md:justify-between flex-col md:flex-row"
   >
-    <MenuItem v-for="link in props.links" :href="link.href">
+    <MenuItem v-for="link in props.links" :key="link.href" :href="link.href">
       {{ link.text }}
     </MenuItem>
   </ul>
