cdc8680de3ad7ef30a198aeaaf302c56c3249d58
Alireza Sadeghi <alirezawdx@gmail.com>
2025-07-06 11:08:21 +0330

    feat: add services item component and layout

---
+++ src/components/ServiceItem.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const { title } = defineProps({
+  title: String,
+  img: String,
+})
+</script>
+
+<template>
+  <div class="p-3 flex-grow">
+    <div
+      class="h-full flex-wrap gap-5 md:flex-nowrap rounded-xl border border-white bg-white p-8 bg-gradient-to-br from-[#fbffff] to-[#e5edfa] flex justify-between items-start"
+    >
+      <div class="flex justify-start items-start flex-col gap-5">
+        <div class="w-12 h-12 rounded-lg bg-[#bce7fa] flex justify-center items-center">
+          <slot name="icon"></slot>
+        </div>
+        <h3 class="mt-2 text-xl text-[#484E53] font-semibold">{{ title }}</h3>
+        <p>
+          <slot name="description"></slot>
+        </p>
+      </div>
+      <div v-if="img" class="h-full w-full">
+        <img :src="img" class="h-full w-full object-cover rounded-lg" :alt="title" />
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/layouts/ServicesList.vue
@@ -0,0 +1,134 @@
+<template>
+  <div class="flex flex-wrap w-full text-left">
+    <ServiceItem title="AI & Deep Learning" class="basis-1/3">
+      <template v-slot:icon>
+        <svg
+          class="w-6 h-6 text-gray-800 dark:text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M12 15v5m-3 0h6M4 11h16M5 15h14a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1Z"
+          />
+        </svg>
+      </template>
+      <template v-slot:description>
+        From data inception to actionable insights, I desifgdhgfhgfdhfghgn compelling analytics and
+        visualization solutions that illuminate trends, empower decision-making, and drive your
+        business forward.
+      </template>
+    </ServiceItem>
+
+    <ServiceItem title="Python Programming" class="basis-1/3">
+      <template v-slot:icon>
+        <svg
+          class="w-6 h-6 text-gray-800 dark:text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-width="2"
+            d="M5 7h14M5 12h14M5 17h14"
+          />
+        </svg>
+      </template>
+      <template v-slot:description>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor rerum quam quae praesentium
+        architecto consequatur ipsam illum adipisci voluptate, excepturi quaerat pariatur optio.
+        Animi, at magnam? Vitae eaque odio officiis!
+      </template>
+    </ServiceItem>
+
+    <ServiceItem title="Design UI" class="basis-1/3">
+      <template v-slot:icon>
+        <svg
+          class="w-6 h-6 text-gray-800 dark:text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill="currentColor"
+            d="m16.1325 4.33451-2.5233-1.40664c-.4721-.26314-1.0678-.1036-1.3452.36025-.5301.88653-.995 1.16623-1.5715 1.51306l-.0109.00653c-.68512.41222-1.48439.89668-2.26786 2.20693-.78556 1.31373-.82234 2.23251-.84978 3.01976l-.00064.0183c-.02242.6445-.04051 1.1644-.56688 2.0447-.53011.8866-.99504 1.1663-1.57156 1.5131l-.01086.0065c-.68515.4122-1.48441.8967-2.26789 2.207-.13881.2321-.17767.5106-.10769.7719.06998.2612.24281.483.47907.6147l2.62175 1.4615c.14127-.4135.30621-.7369.42823-.941.78347-1.3102 1.58276-1.7947 2.26791-2.2069l.01085-.0066c.57652-.3468 1.04146-.6265 1.57155-1.513.5264-.8803.5445-1.4003.5669-2.0448l.0006-.0183c.0275-.7872.0643-1.706.8498-3.01973.7835-1.31024 1.5828-1.79471 2.2679-2.20692l.0109-.00653c.5738-.34523 1.0371-.62397 1.5642-1.50087.1235-.22119.2956-.5451.4544-.87294ZM7.93136 19.6711l2.42894 1.354c.4721.2632 1.0678.1037 1.3452-.3602.5301-.8865.995-1.1662 1.5715-1.513l.0109-.0066c.6852-.4122 1.4844-.8967 2.2679-2.2069.7855-1.3137.8223-2.2325.8498-3.0198l.0006-.0183c.0224-.6444.0405-1.1644.5669-2.0447.5301-.8865.995-1.1662 1.5715-1.5131l.0109-.0065c.6852-.41221 1.4844-.89667 2.2679-2.20692.1388-.23214.1777-.51061.1077-.77188-.07-.26128-.2428-.48306-.479-.61477L17.882 5.30974c-.1678.34035-.34.6625-.4657.88717-.0047.00832-.0095.01658-.0143.02476-.7835 1.31025-1.5828 1.79471-2.2679 2.20693l-.0109.00653c-.5765.34683-1.0415.62653-1.5716 1.51306-.5263.88031-.5444 1.40031-.5668 2.04471l-.0007.0183c-.0274.7873-.0642 1.7061-.8498 3.0198-.7834 1.3102-1.5827 1.7947-2.26785 2.2069l-.01085.0065c-.57651.3469-1.04144.6266-1.57154 1.5131-.10167.17-.25746.4945-.3527.9136Z"
+          />
+        </svg>
+      </template>
+      <template v-slot:description>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor rerum quam quae praesentium
+        architecto consequatur ipsam illum adipisci voluptate, excepturi quaerat pariatur optio.
+        Animi, at magnam? Vitae eaque odio officiis!
+      </template>
+    </ServiceItem>
+
+    <ServiceItem title="Teaching" class="basis-2/3">
+      <template v-slot:icon>
+        <svg
+          class="w-6 h-6 text-gray-800 dark:text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M14.6144 7.19994c.3479.48981.5999 1.15357.5999 1.80006 0 1.6569-1.3432 3-3 3-1.6569 0-3.00004-1.3431-3.00004-3 0-.67539.22319-1.29865.59983-1.80006M6.21426 6v4m0-4 6.00004-3 6 3-6 2-2.40021-.80006M6.21426 6l3.59983 1.19994M6.21426 19.8013v-2.1525c0-1.6825 1.27251-3.3075 2.95093-3.6488l3.04911 2.9345 3-2.9441c1.7026.3193 3 1.9596 3 3.6584v2.1525c0 .6312-.5373 1.1429-1.2 1.1429H7.41426c-.66274 0-1.2-.5117-1.2-1.1429Z"
+          />
+        </svg>
+      </template>
+      <template v-slot:description>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor rerum quam quae praesentium
+        architecto consequatur ipsam illum adipisci voluptate, excepturi quaerat pariatur optio.
+        Animi, at magnam? Vitae eaque odio officiis! Lorem ipsum dolor sit amet consectetur.
+        voluptatem.
+      </template>
+    </ServiceItem>
+
+    <ServiceItem title="Photoshop" class="basis-1/3">
+      <template v-slot:icon>
+        <svg
+          class="w-6 h-6 text-gray-800 dark:text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-width="2"
+            d="M5 7h14M5 12h14M5 17h14"
+          />
+        </svg>
+      </template>
+      <template v-slot:description>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor rerum quam quae praesentium
+        architecto consequatur ipsam illum adipisci voluptate, excepturi quaerat pariatur optio.
+        Animi, at magnam? Vitae eaque odio officiis!
+      </template>
+    </ServiceItem>
+  </div>
+</template>
