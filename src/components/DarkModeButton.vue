7bcb49d6b179b5fff2265821d4fe37040312ee49
Alireza Sadeghi <alirezawdx@gmail.com>
2025-07-06 20:00:48 +0330

    fix: fix condition statement for localstorage checking

--- src/components/DarkModeButton.vue
+++ src/components/DarkModeButton.vue
@@ -11,7 +11,7 @@ const toggleDarkMode = () => {
 }
 
 onMounted(() => {
-  if (localStorage.getItem('darkModeOn') != undefined) {
+  if (localStorage.getItem('darkModeOn') == undefined) {
     localStorage.setItem('darkModeOn', 'on')
   }
   dark.value = localStorage.getItem('darkModeOn') == 'on' ? true : false
